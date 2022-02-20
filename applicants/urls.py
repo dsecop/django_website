cb24580ed7566b5ac51463af34c15a131680b8d9
Dariusz KLimowicz <dariusz.secop@gmail.com>
2022-02-20 20:24:25 +0100

    more urls patterns added

--- applicants/urls.py
+++ applicants/urls.py
@@ -5,4 +5,7 @@ app_name = 'applicants'
 
 urlpatterns = [
     path('dashboard/', views.ApplicantDashboardView.as_view(), name='applicant-dashboard'),
+    path('enroll-task/', views.ApplicantEnrollTaskView.as_view(), name='applicant-enroll-task'),
+    path('task/<int:id>/', views.ApplicantTaskDetailView.as_view(), name='applicant-task-detail'),
+    path('task-list/', views.TaskListView.as_view(), name='all-task-list'),
 ]
